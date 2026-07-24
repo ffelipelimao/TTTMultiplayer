ac629e2057d38e86e3155aaabea4e80c26f35643
Luiz Felipe Limao <lflimao@hotmail.com>
2026-07-23 23:48:49 -0300

    Refactor login functionality and remove server code

--- Assets/Scripts/UI/Login.cs
+++ Assets/Scripts/UI/Login.cs
@@ -1,28 +1,69 @@
+using System.Text.RegularExpressions;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Login : MonoBehaviour
 {
-    private Button _loginButton;
-    private Button _sendButton;
+    private Transform _loginButton;
+    private TMP_InputField _usernameInput;
+    private TMP_InputField _passwordInput;
+    private TextMeshProUGUI _loginText;
+
+    private string _username = string.Empty;
+    private string _password = string.Empty;
+
+    [SerializeField] private int _MaxPasswordLength = 10;
+    [SerializeField] private int _MaxUsernameLength = 10;
+
+
 
     void Start()
     {
-        _loginButton = transform.Find("Connect").GetComponent<Button>();
-        _loginButton.onClick.AddListener(Connect);
+        _loginButton = transform.Find("LoginButton");
+        _loginButton.GetComponent<Button>().onClick.AddListener(LoginAction);
+        _loginText = _loginButton.Find("Text (TMP)").GetComponent<TextMeshProUGUI>();
 
-        _sendButton = transform.Find("Send").GetComponent<Button>();
-        _sendButton.onClick.AddListener(Send);
+        _usernameInput = transform.Find("UsernameInput").GetComponent<TMP_InputField>();
+        _usernameInput.onValueChanged.AddListener(ChangeUsername);
+
+        _passwordInput = transform.Find("PasswordInput").GetComponent<TMP_InputField>();
+        _passwordInput.onValueChanged.AddListener(ChangePassword);
     }
 
-    void Connect()
+    void LoginAction()
     {
-        Client.Instance.Connect();
+        Debug.Log("Logging in...");
     }
-
-    void Send()
+    void ChangeUsername(string value)
     {
-        Client.Instance.SendServer("Hello World!!");
+        _username = value;
+        ValidateAndUpdateUI();
+    }
+    void ChangePassword(string value)
+    {
+        _password = value;
+        ValidateAndUpdateUI();
     }
 
+    void ValidateAndUpdateUI()
+    {
+        var usernameRegex = Regex.Match(_username, "^[a-zA-Z0-9]+$");
+
+        var interactable =
+            !string.IsNullOrWhiteSpace(_username) &&
+            !string.IsNullOrWhiteSpace(_password) &&
+            _username.Length <= _MaxUsernameLength &&
+            _password.Length <= _MaxPasswordLength &&
+            usernameRegex.Success;
+
+        EnableLoginButton(interactable);
+    }
+
+    void EnableLoginButton(bool interactable)
+    {
+        _loginButton.GetComponent<Button>().interactable = interactable;
+        var color = _loginButton.GetComponent<Button>().interactable ? Color.white : Color.grey;
+        _loginText.color = color;
+    }
 }
